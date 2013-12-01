5bae38e89914c1899329689a63c23644f79edcc8
Robert Kirberich <robert@kirberich.de>
2013-12-01 19:52:06 +0000

    Add wiimote support

--- attentionbot.py
+++ attentionbot.py
@@ -2,6 +2,7 @@ import time
 
 from api import Api
 from motors import Servo, Motor
+from remote import Remote
 
 servo = Servo(port=1, min_pulse=750, max_pulse=2250, min_position=0.37, max_position=0.63)
 motor = Motor(port=1, duty_cycle=0)
@@ -16,8 +17,10 @@ def wave(min=0.0, max=1.0, n=1, pause=0.4):
 
 api = Api()
 api.demonize()
+remote = Remote()
 
 while True:
+    # api events
     for event, kwargs in api.events:
         if event == "wave":
             wave(**kwargs)
@@ -26,5 +29,12 @@ while True:
         if event == "set_speed":
             motor.duty_cycle = kwargs['position']
     api.events = []
+
+    # remote events
+    if remote.pressed('a'):
+        remote.rumble(True)
+    else:
+        remote.rumble(False)
+
     motor.tick()
     #time.sleep(0.01)
---
+++ remote.py
@@ -0,0 +1,59 @@
+import cwiid
+
+BUTTONS = {
+    'up': 2048,
+    'down': 1024,
+    'left': 256,
+    'right': 512,
+    'a': 8,
+    'b': 4,
+    'home': 128,
+    'minus': 16,
+    'plus': 4096,
+    '1': 2,
+    '2': 1
+}
+
+class Remote(object):
+    def __init__(self):
+        self.wm = None
+        self.accel_calibration = (0, 0, 0)
+
+        tries = 0
+        while not self.wm:
+            try:
+                self.wm = cwiid.Wiimote()
+                self.wm.rpt_mode = cwiid.RPT_BTN | cwiid.RPT_ACC | cwiid.RPT_NUNCHUK
+                self.wm.led = 1
+            except RuntimeError:
+                if tries > 10:
+                    return
+            finally:
+                tries += 1
+
+    def pressed(self, button=None, buttons=None):
+        button_index = 0
+        if button:
+            button_index = BUTTONS[button.lower()]
+        if buttons:
+            for _button in buttons:
+                button_index = button_index | BUTTONS[_button_name.lower()]
+
+        return self.wm.state['buttons'] & button_index
+
+    def accel(self):
+        return self.wm.state['acc'] - self.accel_calibration
+
+    def calibrate(self):
+        _avg = (0, 0, 0)
+
+        for x in range(0, 20):
+            _avg += self.wm.state['acc']
+        self.accel_calibration = (_avg[0]/20.0, _avg[1]/20.0, _avg[2]/20.0)
+        print "calibrated."
+
+    def rumble(self, state):
+        self.wm.rumble = state
+
+    def toggle_rumble(self):
+        self.wm.rumble = not self.wm.rumble
